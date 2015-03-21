267c8ad161a8cf7675477fc373d18a059e9c2588
cgui@usc.edu <tedatworking@gmail.com>
2015-03-21 00:21:21 -0700

    Add new file

---
+++ Algorithm/Sort/SmallestMissingPositiveInteger.java
@@ -0,0 +1,11 @@
+/*
+Find the smallest positive integer value that cannot be represented as sum of any subset of a given array
+Given a sorted array (sorted in non-decreasing order) of positive numbers, find the smallest positive integer value that cannot be represented as sum of elements of any subset of given set. 
+Expected time complexity is O(n).
+*/
+
+class SmallestMissingPositiveInteger {
+
+
+	
+}
